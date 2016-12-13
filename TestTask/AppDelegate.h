8f6d94d16c7a22d516c5a6484a3172b827cd045b
theFluterr <f.l.i.t.e1.9.9.6@gmail.com>
2016-12-13 18:46:58 +0200

    Project setup

---
+++ TestTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTask
+//
+//  Created by Andrei on 13.12.16.
+//  Copyright © 2016 Tesk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
